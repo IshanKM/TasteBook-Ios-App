44389464e5950ee2ff42887b510e58b361cc504e
IshanKM <ishanmadushanka.sg@gmail.com>
2024-10-02 00:49:43 -0700

    first commit

---
+++ TasteBook/TasteBook/TasteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasteBookApp.swift
+//  TasteBook
+//
+//  Created by IKM on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TasteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
